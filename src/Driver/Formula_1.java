7eab223b2f6e0cdeea45ec16d08042d870e58fea
Dmytro Hultiaev <work.gultiaev@gmail.com>
2022-11-18 19:55:53 +0200

    New package driver

---
+++ src/Driver/Formula_1.java
@@ -0,0 +1,13 @@
+package Driver;
+
+public class Formula_1 {
+	private String FIO;
+	private String team;
+	private int experience;
+	
+	public void print() {
+		System.out.println(FIO);
+		System.out.println(team);
+		System.out.println(experience);
+	}
+}
